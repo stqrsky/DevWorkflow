557fab982b7f3e709227825bee58bb81144cfb94
star <star@starbook.fritz.box>
2020-07-16 19:11:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <app-servers></app-servers>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<style>
+  
+</style>
