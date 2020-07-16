33317dd68f761a2717ffd1e41e4ea570cdbcff46
star <star@starbook.fritz.box>
2020-07-16 20:24:14 +0200

    restructured

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,10 @@
 </template>
 
 <script>
-    import Header from './components/Header.vue';
-    import Footer from './components/Footer.vue';
-    import Servers from './components/Servers.vue';
-    import ServerDetails from './components/ServerDetails.vue';
+    import Header from './components/Shared/Header.vue';
+    import Footer from './components/Shared/Footer.vue';
+    import Servers from './components/Server/Servers.vue';
+    import ServerDetails from './components/Server/ServerDetails.vue';
 
     export default {
         components: {
