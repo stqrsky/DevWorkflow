32d5d56cd54884872c184805e5e19de83966f5d8
star <star@starbook.fritz.box>
2020-07-21 07:46:17 +0200

    dot

--- src/components/Shared/Footer.vue
+++ src/components/Shared/Footer.vue
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-xs-12">
             <footer>
-                <p>All Servers are managed here</p>
+                <p>All Servers are managed here.</p>
             </footer>
         </div>
     </div>
