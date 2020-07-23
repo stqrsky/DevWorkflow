671c1bbd5d2b2a5b532a2cd612db07f3e88ac19f
star <star@starbook.fritz.box>
2020-07-23 16:04:20 +0200

    button to change status

--- src/components/Server/ServerDetails.vue
+++ src/components/Server/ServerDetails.vue
@@ -2,6 +2,8 @@
     <div class="col-xs-12 col-sm-6">
         <p v-if="!server">Please select a Server</p>
         <p v-else>Server #{{ server.id }} selected, Status: {{ server.status }}</p>
+        <hr>
+        <button @click="resetStatus">Change to Normal</button>
     </div>
 
 </template>
@@ -15,6 +17,11 @@
                 server: null
             }
         },
+        methods: {
+            resetStatus() {
+                this.server.status = 'Normal';
+            } 
+        }
         created() {
             serverBus.$on('serverSelected', (server) => {
                 this.server = server;
